528a749da1abbb02da88196361b572f2029be07e
amuthas <122219265+amutharaj0597@users.noreply.github.com>
2025-09-05 10:16:22 +0530

    index.js

--- ticketing-system/index.js
+++ ticketing-system/index.js
@@ -7,7 +7,6 @@ const ticketRoutes = require('./routes/ticketRoutes');
 app.use(express.json());
 app.use('/tickets', ticketRoutes);
 
-// 404 Route handler
 app.use((req, res) => {
   res.status(404).json({ message: '404 Not Found' });
 });
