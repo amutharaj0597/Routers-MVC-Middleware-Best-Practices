f5e302134f45e180a05cc0c2f294e6339a454bfe
amuthas <122219265+amutharaj0597@users.noreply.github.com>
2025-09-05 10:17:49 +0530

    index.js

--- employee-management-backend/index.js
+++ employee-management-backend/index.js
@@ -5,14 +5,11 @@ const loggerMiddleware = require('./middlewares/loggerMiddleware');
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(express.json());
 app.use(loggerMiddleware);
 
-// Routes
 app.use('/employees', employeeRoutes);
 
-// 404 Handler
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
